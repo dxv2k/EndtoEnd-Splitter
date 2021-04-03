249d1045585cb686ea9b628b69e992ffeab96c22
VinhDevNguyen <vinh2000ld14@gmail.com>
2021-04-03 14:39:33 +0700

    Create API call file path

--- server.py
+++ server.py
@@ -1,43 +1,11 @@
+from fastapi import FastAPI 
 from typing import Optional
-
-from fastapi import FastAPI
-
-# async def app(scope, receive, send):
-#     assert scope['type'] == 'http'
-
-#     await send({
-#         'type': 'http.response.start',
-#         'status': 200,
-#         'headers': [
-#             [b'content-type', b'text/plain'],
-#         ],
-#     })
-#     await send({
-#         'type': 'http.response.body',
-#         'body': b'Hello, world!',
-#     })
-
-
 app = FastAPI()
 
-# for testing purpose
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/") 
+async def root(): 
+    return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.route("/predict",method = "POST",)
-def predict()
-    # get audio file and save it
-    # invoke keyword spotting service 2stems, 4stems, 5stems 
-    # make prediction by invoke CLI args    
-    # send back the predicted files
-    pass
-
-
-
-if __name__ == "__main__":
-    app.run(debug=False)
+@app.get("/audio/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
